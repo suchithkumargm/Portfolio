c758ed130e28986353a04143025b28e3f1ddbc37
Suchith Kumar G M <suchithkumargm@gmail.com>
2023-07-18 22:32:36 +0530

    completed About section with AppWrap and HOC

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import {AiFillEye,AiFillGithub} from 'react-icons/ai';
+import {motion} from 'framer-motion';
 
+import {AppWrapper} from '../../wrapper';
+import {urlFor,client} from '../../client.js';
 import './Work.scss';
 
 const Work = () => {
   return (
-    <div>
-      Work
-    </div>
+    <>
+    <h2 className='head-text'>I know that <span>Good Dev</span><br /> means  <span>Good Business</span></h2>
+    </>
   )
 }
 
