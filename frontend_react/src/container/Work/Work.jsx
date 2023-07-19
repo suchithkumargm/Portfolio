db03a32186a6360059e551743014b1d2abcfa182
Suchith Kumar G M <suchithkumargm@gmail.com>
2023-07-19 11:53:34 +0530

    completed work with works filtering

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -12,19 +12,30 @@ const Work = () => {
 	const [works, setWorks] = useState([])
 	const [filterWork, setFilterWork] = useState([])
 
-	useEffect(()=>{
-		const query='*[_type == "works"]';
+	useEffect(() => {
+		const query = '*[_type == "works"]';
 
 		client.fetch(query)
-		.then((data)=>{
-			setWorks(data)
-			setFilterWork(data)
-		})
+			.then((data) => {
+				setWorks(data)
+				setFilterWork(data)
+			})
 
-	},[]);
+	}, []);
 
 	const handleWorkFilter = (item) => {
+		setActiveFilter(item);
+		setAnimateCard([{ y: 100, opacity: 0 }]);
 
+		setTimeout(() => {
+			setAnimateCard([{ y: 0, opacity: 1 }]);
+
+			if (item === 'All') {
+				setFilterWork(works);
+			} else {
+				setFilterWork(works.filter((work)=>work.tags.includes(item)));
+			}
+		}, 500);
 	}
 
 	return (
@@ -32,7 +43,7 @@ const Work = () => {
 			<h2 className='head-text'>My creative <span>Portfolio</span><br /> section.</h2>
 
 			<div className='app__work-filter'>
-				{['MERN', 'java', 'backend', 'frontend', 'databse','All'].map((item, index) =>
+				{['MERN', 'java', 'backend', 'frontend', 'databse', 'All'].map((item, index) =>
 					<div
 						key={index}
 						onClick={() => handleWorkFilter(item)}
@@ -44,36 +55,36 @@ const Work = () => {
 			</div>
 
 			<motion.div
-			animate={animateCard}
-			transition={{duration:0.5,delayChildren:0.5}}
-			className='app__work-portfolio'
+				animate={animateCard}
+				transition={{ duration: 0.5, delayChildren: 0.5 }}
+				className='app__work-portfolio'
 			>
-				{filterWork.map((work,index)=>(
+				{filterWork.map((work, index) => (
 					<div className='app__work-item app__flex' key={index}>
 						<div className='app__work-img app__flex'>
-							<img src={urlFor(work.imgUrl)} alt={work.name}/>
+							<img src={urlFor(work.imgUrl)} alt={work.name} />
 
 							<motion.div
-							whileHover={{opacity:[0,1]}}
-							transition={{duration:0.25,ease:'easeInOut' ,staggerChildren:0.5}}
-							className='app__work-hover app__flex'
+								whileHover={{ opacity: [0, 1] }}
+								transition={{ duration: 0.25, ease: 'easeInOut', staggerChildren: 0.5 }}
+								className='app__work-hover app__flex'
 							>
 								<a href={work.projectLink} target='_blank' rel='norefer'>
-									<motion.div 
-									whileInView={{scale:[0,1]}}
-									whileHover={{scale:[1,0.9]}}
-									transition={{duration:0.25}}
-									className='app__flex'
+									<motion.div
+										whileInView={{ scale: [0, 1] }}
+										whileHover={{ scale: [1, 0.9] }}
+										transition={{ duration: 0.25 }}
+										className='app__flex'
 									>
 										<AiFillEye />
 									</motion.div>
 								</a>
 								<a href={work.projectLink} target='_blank' rel='norefer'>
-									<motion.div 
-									whileInView={{scale:[0,1]}}
-									whileHover={{scale:[1,0.9]}}
-									transition={{duration:0.25}}
-									className='app__flex'
+									<motion.div
+										whileInView={{ scale: [0, 1] }}
+										whileHover={{ scale: [1, 0.9] }}
+										transition={{ duration: 0.25 }}
+										className='app__flex'
 									>
 										<AiFillGithub />
 									</motion.div>
@@ -83,7 +94,7 @@ const Work = () => {
 
 						<div className='app__work-content app__flex'>
 							<h4 className='bold-text'>{work.title}</h4>
-							<p className='p-text' style={{marginTop:10}}>{work.description}</p>
+							<p className='p-text' style={{ marginTop: 10 }}>{work.description}</p>
 
 							<div className='app__work-tag app__flex'>
 								<p className='p-text'>{work.tags[0]}</p>
